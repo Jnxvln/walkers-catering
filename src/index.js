51d017a93afb7e94e636b4fee0fecb3dc34eff4e
Justin Cox <justincox@Violmusio.attlocal.net>
2022-01-10 20:58:27 -0600

    Remove the Strict mode element from around App component in index.js

--- src/index.js
+++ src/index.js
@@ -10,9 +10,7 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
