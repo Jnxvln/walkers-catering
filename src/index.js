44383ca7b7c1ea42f4342671407391c51cbd46a8
Justin Cox <justincox@Violmusio.attlocal.net>
2022-01-10 20:19:17 -0600

    Import PrimeReact CSS files inside index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
