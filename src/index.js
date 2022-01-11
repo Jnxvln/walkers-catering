8acc7eb412ebc5fa1c496a87c44733ff446ea4cd
Justin Cox <justincox@Violmusio.attlocal.net>
2022-01-10 20:23:31 -0600

    Add PrimeFlex CSS to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
 import "primereact/resources/primereact.min.css";                  //core css
 import "primeicons/primeicons.css";                                //icons
+import 'primeflex/primeflex.css'
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
